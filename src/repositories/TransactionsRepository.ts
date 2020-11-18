ff14b842f3f83f2e6d2077358f59e4bb0c5fb0e3
Sandro Vieira <vsandro@gmail.com>
2020-11-18 20:17:20 -0300

    update versao 1.0

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransaction {
+    title: string
+    value: number
+    type: string
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +20,61 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions
+  }
+
+  sumIncome() {
+    const tipo = this.transactions.filter( function( elem, index, array ) {
+        return elem.type === 'income'
+    } );
+
+    const valores = tipo.map( function( elem ) {
+      return elem.value
+    })
+
+    var total = valores.reduce(function(total, numero) {
+      return total + numero
+    }, 0)
+
+    return total
+  }
+
+  sumOutcome() {
+    const tipo = this.transactions.filter( function( elem, index, array ) {
+        return elem.type === 'outcome'
+    } );
+
+    const valores = tipo.map( function( elem ) {
+      return elem.value
+    })
+
+    var total = valores.reduce(function(total, numero) {
+      return total + numero
+    }, 0)
+
+    return total
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.sumIncome()
+    const outcome = this.sumOutcome()
+    const total = income - outcome
+
+    const balance = {
+      "income": income,
+      "outcome": outcome,
+      "total": total
+    }
+
+    return balance
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransaction): Transaction {
+    const transaction = new Transaction({ title, value, type })
+
+    this.transactions.push(transaction)
+
+    return transaction
   }
 }
 
